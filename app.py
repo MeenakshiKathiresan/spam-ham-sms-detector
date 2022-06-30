fca2c3d9c74d4b9fb539f7784a5262fbfe6edb82
meenakshi-kathiresan <meenakshi@hammerplay.com>
2022-06-30 12:30:10 -0400

    get sms from flask

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, request,render_template, make_response
 from twilio.rest import Client
+from twilio import twiml
 import pickle
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def index():
-    message = 'Hey there!' #twilio_api.messages.stream()
 
-    #message = request.values.get('Body', None)
-    #sender = request.values.get('From', None)
+    message = 'Hey there!' 
+
+    sender = request.form['From']
+    message = request.form['Body']
 
     new_model_file = open('model.pkl', 'rb')
     new_model=pickle.load(new_model_file)
 
     prediction = (new_model.predict([message])[0], max(new_model.predict_proba([message])) * 100)
-    #reply = <Response><Message> prediction </Response></Message>
-    reply = prediction
-    return render_template('index.html', prediction=prediction, sms=message) 
+
+    resp = twiml.Response()
+    resp.message(prediction)
+
+    return str(resp)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=4000, debug=True)
