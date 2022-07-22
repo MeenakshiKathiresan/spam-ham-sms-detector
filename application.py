b138b940a3690e05edd209f609bca02dc5ad9cdb
meenakshi-kathiresan <meenakshi@hammerplay.com>
2022-07-22 17:37:10 -0400

    capture response

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import boto3
 import datetime
+from boto3.dynamodb.conditions import Key
 
 import pickle
 import os
@@ -31,24 +32,51 @@ def sms_reply():
     sender = request.form['From']
     message = request.form['Body']
 
-    new_model_file = open('model.pkl', 'rb')
-    new_model=pickle.load(new_model_file)
-    prediction = (new_model.predict([message])[0])
+
 
  
     table = dynamodb.Table('sms_classification_logs')
+
+    response = table.query(
+            KeyConditionExpression=Key('Sender').eq(sender)
+            )
+    items = response['Items']
+    valid_msg = ""
+    clean_msg = message.strip().lower()
+    if len(items)>0 and (clean_msg == 'correct' or clean_msg == 'wrong'):
+        last_item = items[len(items)-1]
+        valid_msg = last_item['Validation']
+        if valid_msg == '':
+            if clean_msg == 'correct':
+                prediction = "Thank you for confirming."
+            elif clean_msg == 'wrong':
+                prediction = "Oops! Thank you for helping us get better."
+            table.put_item(Item={
+                'Validation': message, 
+                'Sender': sender, 
+                'DateTime': last_item['DateTime'],
+                'Message':last_item['Message'] ,
+                'Prediction': last_item['Prediction']})
+
+    else:
+        new_model_file = open('model.pkl', 'rb')
+        new_model=pickle.load(new_model_file)
+        prediction = (new_model.predict([message])[0])
+
+        table.put_item(
+                Item={
+        'Message': message,
+        'Prediction': prediction,
+        'Sender': sender,
+        'DateTime': str(datetime.datetime.now()),
+        'Validation': ''
+            }
+        )
+        prediction = "The message sent is predicted as " + prediction + "\nValidate by sending 'correct' or 'wrong'."
+
+    
         
-    table.put_item(
-            Item={
-    'Message': message,
-    'Prediction': prediction,
-    'Sender': sender,
-    'DateTime': str(datetime.datetime.now())
-        }
-    )
-
-    prediction = "The message sent is predicted as " + prediction 
-
+    
     resp = MessagingResponse()
     resp.message(str(prediction))
 
